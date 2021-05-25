386917d7bd507e0ed2e3fb388012d4850189f384
= <=>
2021-05-25 12:35:21 -0700

    css js html server all working

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const pug = require('pug');
+const webpack = require('webpack');
+const routes = require('./index');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3333;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/', routes);
+
 app.get('/', (req, res) => {
-    res.send('Hello world');
 });
 
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded);
