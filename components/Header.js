7777c47e6475c8340419dc2220077678970c70a4
Erick Askim <erickaskim@gmail.com>
2021-07-30 11:48:04 -0700

    add header component

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import Nav from './Nav'
+import Link from 'next/link'
+
+export default function Header() {
+    return (
+        <header>
+            <div className='header'>
+                <Link href='/'>Erick Askim</Link>
+            </div>
+            <Nav />
+        </header>
+    )
+}
