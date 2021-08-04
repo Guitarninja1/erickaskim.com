549f5b4322fa270330950d530fbb07eb5831ef5f
Erick Askim <erickaskim@gmail.com>
2021-08-04 16:08:59 -0700

    add onclick functionality for menu

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,22 @@
 import Link from 'next/link';
+import ExitB from './ExitB';
+import MagGlass from './MagGlass';
 
-export default function Nav() {
+export default function Nav(props) {
+	const { open, setOpen } = props;
 	return (
-		<nav>
+		<nav
+			className={`nav--menu ${
+				open ? 'nav--menu__on_screen' : 'nav--menu__off_screen'
+			}`}
+		>
+			<button className='exit-button' onClick={() => setOpen(!open)}>
+				<ExitB />
+			</button>
 			<ul>
+				<li>
+					<Link href='/'>Home</Link>
+				</li>
 				<li>
 					<Link href='/blog'>Blog</Link>
 				</li>
@@ -13,6 +26,11 @@ export default function Nav() {
 				<li>
 					<Link href='/contact'>Contact</Link>
 				</li>
+				<input
+					className='search'
+					type='search'
+					defaultValue='Search...                 '
+				></input>
 			</ul>
 		</nav>
 	);
