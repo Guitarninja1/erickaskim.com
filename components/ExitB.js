cc61ff83a03c0026dc236c56ac7c1ad696414882
Erick Askim <erickaskim@gmail.com>
2021-08-04 16:04:19 -0700

    add css and icons

---
+++ components/ExitB.js
@@ -0,0 +1,19 @@
+export default function ExitB() {
+	return (
+		<svg
+			width='31'
+			height='30'
+			viewBox='0 0 31 30'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<path
+				fillRule='evenodd'
+				clipRule='evenodd'
+				d='M15.2842 15L29.4264 29.1422L30.1335 28.4351L15.9913 14.2929L29.4264 0.85782L28.7193 0.150713L15.2842 13.5858L1.8492 0.150802L1.14209 0.857909L14.5771 14.2929L0.434984 28.435L1.14209 29.1421L15.2842 15Z'
+				fill='white'
+				fillOpacity='0.3'
+			/>
+		</svg>
+	);
+}
