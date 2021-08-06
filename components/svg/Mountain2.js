60c95e50fd9c559ec7f456a828ac100e38abd682
Erick Askim <erickaskim@gmail.com>
2021-08-06 14:01:38 -0700

    add images and styling

---
+++ components/svg/Mountain2.js
@@ -0,0 +1,54 @@
+export default function Mountain2() {
+	return (
+		<svg
+			width='360'
+			height='408'
+			viewBox='0 0 360 408'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<g filter='url(#filter0_d)'>
+				<path
+					d='M0 206.348C0 206.348 6.8764 666.097 14.5618 379.908C22.2472 93.7196 21.0337 538.696 39.0337 206.348C57.0337 -126 93.6404 477.421 104.157 246.624C114.674 15.8269 136.517 307.899 153.91 145.418C171.303 -17.0635 188.899 306.236 196.18 337.624C203.461 369.013 220.652 366.983 231.169 263.586C241.685 160.189 240.27 494.969 273.438 411.296C306.607 327.624 299.326 -20.7562 328.652 145.418C357.978 311.592 360 302.36 360 302.36V575.624H0V206.348Z'
+					fill='#0F1118'
+				/>
+			</g>
+			<defs>
+				<filter
+					id='filter0_d'
+					x='-90'
+					y='0'
+					width='500'
+					height='615.624'
+					filterUnits='userSpaceOnUse'
+					colorInterpolationFilters='sRGB'
+				>
+					<feFlood floodOpacity='0' result='BackgroundImageFix' />
+					<feColorMatrix
+						in='SourceAlpha'
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+						result='hardAlpha'
+					/>
+					<feOffset dx='-20' dy='-30' />
+					<feGaussianBlur stdDeviation='35' />
+					<feColorMatrix
+						type='matrix'
+						values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0'
+					/>
+					<feBlend
+						mode='normal'
+						in2='BackgroundImageFix'
+						result='effect1_dropShadow'
+					/>
+					<feBlend
+						mode='normal'
+						in='SourceGraphic'
+						in2='effect1_dropShadow'
+						result='shape'
+					/>
+				</filter>
+			</defs>
+		</svg>
+	);
+}
