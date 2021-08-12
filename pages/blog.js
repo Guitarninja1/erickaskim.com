e08eb8393150389baf5bf2b94e017a4dc74e7308
Guitarninja1 <77123368+Guitarninja1@users.noreply.github.com>
2021-08-12 12:38:09 -0700

    add content (#7)
    * add content
    
    * address issues 2
    
    * address pr request III

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,99 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import {
+	FacebookIcon,
+	FacebookShareButton,
+	TwitterIcon,
+	TwitterShareButton,
+} from 'react-share';
+
 export default function blog() {
 	return (
-		<div>
-			<h2>Blog</h2>
-			<p className='blog--p'>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget erat
-				luctus, condimentum lectus eu, egestas tortor. Donec posuere vestibulum
-				eros. Pellentesque elementum eleifend mollis. Fusce ac risus ut mauris
-				imperdiet dapibus. Fusce non libero nec nisl facilisis posuere. Donec
-				gravida ex at arcu bibendum, in porta quam viverra.
-			</p>
+		<div className='blog--wrapper'>
+			<div className='blog--items'>
+				<h2>Blog</h2>
+				<Image
+					src='https://picsum.photos/200/300'
+					height='100%'
+					width='100%'
+					alt='image'
+				/>
+				<p className='blog--p'>Erick Askim</p>
+				<p className='blog--p'>Aug 12,2021</p>
+				<FacebookShareButton url='#'>
+					<FacebookIcon size={32} round={true} />
+				</FacebookShareButton>
+				<TwitterShareButton url='#'>
+					<TwitterIcon size={32} round={true} />
+				</TwitterShareButton>
+				<p className='blog--p'>Lorem ipsum</p>
+				<Link href='#'>
+					<a aria-label='Next'>→</a>
+				</Link>
+			</div>
+			<div className='blog--items'>
+				<h2>Blog</h2>
+				<Image
+					src='https://picsum.photos/200/300'
+					height='100%'
+					width='100%'
+					alt='image'
+				/>
+				<p className='blog--p'>Erick Askim</p>
+				<p className='blog--p'>Aug 11,2021</p>
+				<FacebookShareButton url='#'>
+					<FacebookIcon size={32} round={true} />
+				</FacebookShareButton>
+				<TwitterShareButton url='#'>
+					<TwitterIcon size={32} round={true} />
+				</TwitterShareButton>
+				<p className='blog--p'>Lorem ipsum</p>
+				<Link href='#'>
+					<a aria-label='Next'>→</a>
+				</Link>
+			</div>
+			<div className='blog--items'>
+				<h2>Blog</h2>
+				<Image
+					src='https://picsum.photos/200/300'
+					height='100%'
+					width='100%'
+					alt='image'
+				/>
+				<p className='blog--p'>Erick Askim</p>
+				<p className='blog--p'>Aug 10,2021</p>
+				<FacebookShareButton url='#'>
+					<FacebookIcon size={32} round={true} />
+				</FacebookShareButton>
+				<TwitterShareButton url='#'>
+					<TwitterIcon size={32} round={true} />
+				</TwitterShareButton>
+				<p className='blog--p'>Lorem ipsum</p>
+				<Link href='#'>
+					<a aria-label='Next'>→</a>
+				</Link>
+			</div>
+			<div className='blog--items'>
+				<h2>Blog</h2>
+				<Image
+					src='https://picsum.photos/200/300'
+					height='100%'
+					width='100%'
+					alt='image'
+				/>
+				<p className='blog--p'>Erick Askim</p>
+				<p className='blog--p'>Aug 10,2021</p>
+				<FacebookShareButton url='#'>
+					<FacebookIcon size={32} round={true} />
+				</FacebookShareButton>
+				<TwitterShareButton url='#'>
+					<TwitterIcon size={32} round={true} />
+				</TwitterShareButton>
+				<p className='blog--p'>Lorem ipsum</p>
+				<Link href='#'>
+					<a aria-label='Next'>→</a>
+				</Link>
+			</div>
 		</div>
 	);
 }
