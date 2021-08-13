98a09d2c16c2077cc4134780e850e37adcb1bb99
Erick Askim <erickaskim@gmail.com>
2021-08-13 13:21:10 -0700

    404 content

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function FourZeroFour() {
 	return (
-		<div className='404--wrapper'>
-			<h1 className='404--oops'>OOPS!</h1>
+		<div className='fourzerofour--wrapper'>
+			<h1 className='fourzerofour--title'>OOPS!</h1>
+			<ul className='fourzerofour--ul'>
+				<li>
+					<Link href='/'>Home</Link>
+				</li>
+				<li>
+					<Link href='/'>404</Link>
+				</li>
+			</ul>
+			<div className='fourzerofour--error'>
+				<h2 className='fourzerofour--h2'>404</h2>
+				<h3 className='fourzerofour--h3'>Page not found</h3>
+			</div>
+			<p>
+				It looks like nothing was found at this location. Maybe try a search?
+			</p>
+			<input type='search' placeholder='Search' />
 		</div>
 	);
 }
