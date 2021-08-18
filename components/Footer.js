f467ebbd7478cb526ab0d2a97f819ac0ea4b03c2
Erick Askim <erickaskim@gmail.com>
2021-08-18 13:57:56 -0700

    fix footer menu

--- components/Footer.js
+++ components/Footer.js
@@ -1,52 +1,53 @@
 import Link from 'next/link';
+import styles from './Footer.module.css';
 
 export default function Footer() {
 	const year = new Date().getFullYear();
 	return (
-		<div className='footer--wrapper'>
+		<div className={styles.wrapper}>
 			<div className='footer--menu'>
-				<h2 className='footer--title'>Erick Askim</h2>
-				<ul className='footer--menu-ul'>
+				<h2 className={styles.title}>Erick Askim</h2>
+				<ul className={styles.menu}>
 					<li>
 						<Link href='/'>
-							<a className='footer--p'>Home</a>
+							<a className={styles.menuli}>Home</a>
 						</Link>
 					</li>
 					<li>
 						<Link href='/about'>
-							<a className='footer--p'>About</a>
+							<a className={styles.menuli}>About</a>
 						</Link>
 					</li>
 					<li>
 						<Link href='/'>
-							<a className='footer--p'>Services</a>
+							<a className={styles.menuli}>Services</a>
 						</Link>
 					</li>
 					<li>
 						<Link href='/'>
-							<a className='footer--p'>Portfolio</a>
+							<a className={styles.menuli}>Portfolio</a>
 						</Link>
 					</li>
 					<li>
 						<Link href='/blog'>
-							<a className='footer--p'>Blog</a>
+							<a className={styles.menuli}>Blog</a>
 						</Link>
 					</li>
 					<li>
 						<Link href='/contact'>
-							<a className='footer--p'>Contact</a>
+							<a className={styles.menuli}>Contact</a>
 						</Link>
 					</li>
 				</ul>
 			</div>
 			<div className='footer--info'>
-				<div className='footer--address'>
+				<div className={styles.address}>
 					<h3 className='footer--h3'>Address:</h3>
 					<address className='footer--p'>
 						536 W La Strada Circle, Medford, OR 96504, USA
 					</address>
 				</div>
-				<div className='footer--contact'>
+				<div className={styles.contact}>
 					<h3 className='footer--h3'>Contact</h3>
 					<Link href='tel:+15308282706'>
 						<a className='footer--p'>+1 530 828 2706</a>
@@ -55,7 +56,7 @@ export default function Footer() {
 						<a className='footer--p'>erickaskim@gmail.com</a>
 					</Link>
 				</div>
-				<div className='footer--usefull-links'>
+				<div className={styles.usefull}>
 					<h3 className='footer--h3'>Usefull Links</h3>
 					<Link href='/'>
 						<a className='footer--p'>Help And Support</a>
