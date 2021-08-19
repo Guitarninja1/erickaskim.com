915334ff67528cd330220720b3adaef559ff05e5
Erick Askim <erickaskim@gmail.com>
2021-08-19 14:21:48 -0700

    about page

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
 	images: {
 		domains: ['picsum.photos'],
 	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
 };
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,11 @@ export default class MyDocument extends Document {
 			<Html lang='en-US'>
 				<Head>
 					<link rel='preconnect' href='https://fonts.googleapis.com' />
-					<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+					<link
+						rel='preconnect'
+						href='https://fonts.gstatic.com'
+						crossOrigin='true'
+					/>
 					<link
 						href='https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&family=Raleway:wght@300;400;500;600;700;800;900&display=swap'
 						rel='stylesheet'
