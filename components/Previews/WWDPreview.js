fea53f9e582601370465f8e5b8b57eeb3bc1cd6f
Erick Askim <erickaskim@gmail.com>
2021-09-15 12:19:33 -0700

    what we do css MarkI

--- components/Previews/WWDPreview.js
+++ components/Previews/WWDPreview.js
@@ -7,39 +7,36 @@ export default function WWDPreview() {
 		<div className={styles.wrapper}>
 			<div className='wwdpreview--head'>
 				<Flavortext title='What We Do' />
-				<p>
+				<h2 className={styles.PageTitle}>We Develop Digital Products</h2>
+				<p className={styles.Text}>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
 					fringilla facilisis tortor non eleifend. Ut.
 				</p>
 			</div>
 			<div className='wwdpreview--items'>
 				<WhatWeDo
+					circle='130px'
+					percent='63%'
 					title='Branding'
-					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-							fringilla facilisis tortor non eleifend. Ut.'
-					image='https://picsum.photos/200/300'
-					imageAlt='alt'
+					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
 				/>
 				<WhatWeDo
+					circle='75px'
+					percent='87%'
 					title='Strategy'
-					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-							fringilla facilisis tortor non eleifend. Ut.'
-					image='https://picsum.photos/200/300'
-					imageAlt='alt'
+					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
 				/>
 				<WhatWeDo
+					circle='80px'
+					percent='85%'
 					title='ECommerce'
-					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-							fringilla facilisis tortor non eleifend. Ut.'
-					image='https://picsum.photos/200/300'
-					imageAlt='alt'
+					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
 				/>
 				<WhatWeDo
+					circle='50px'
+					percent='99%'
 					title='Support'
-					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-							fringilla facilisis tortor non eleifend. Ut.'
-					image='https://picsum.photos/200/300'
-					imageAlt='alt'
+					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
 				/>
 			</div>
 		</div>
--- components/WhatWeDo.js
+++ components/WhatWeDo.js
@@ -1,11 +1,15 @@
-import Image from 'next/image';
+import PercentCircle from './PercentCircle';
 import styles from './WhatWeDo.module.css';
 
 export default function WhatWeDo(props) {
-	const { title, text, image, imageAlt } = props;
+	const { title, text, percent, circle } = props;
 	return (
 		<div className={styles.WhatWeDo}>
-			<Image src={image} alt={imageAlt} height='100%' width='100%' />
+			<PercentCircle
+				percent={percent}
+				circle={circle}
+				className={styles.PercentCircle}
+			/>
 			<div className={styles.TextWrap}>
 				<h3 className={styles.Title}>{title}</h3>
 				<p className={styles.Text}>{text}</p>
