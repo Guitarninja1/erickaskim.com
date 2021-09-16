64d01709ddc57c0952ae4c28c991c3e9dab572dc
Erick Askim <erickaskim@gmail.com>
2021-09-16 11:07:57 -0700

    pr response

--- components/Previews/WWDPreview.js
+++ components/Previews/WWDPreview.js
@@ -4,7 +4,7 @@ import styles from './WWDPreview.module.css';
 
 export default function WWDPreview() {
 	return (
-		<div className={styles.wrapper}>
+		<div className={styles.Wrapper}>
 			<div className='wwdpreview--head'>
 				<Flavortext title='What We Do' />
 				<h2 className={styles.PageTitle}>We Develop Digital Products</h2>
@@ -13,27 +13,28 @@ export default function WWDPreview() {
 					fringilla facilisis tortor non eleifend. Ut.
 				</p>
 			</div>
-			<div className='wwdpreview--items'>
+			<div className={styles.WhatWeDoWrap}>
 				<WhatWeDo
-					circle='130px'
+					className={styles.WWDItem}
+					PercentGap='-100px'
 					percent='63%'
 					title='Branding'
 					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
 				/>
 				<WhatWeDo
-					circle='75px'
+					PercentGap='-33px'
 					percent='87%'
 					title='Strategy'
 					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
 				/>
 				<WhatWeDo
-					circle='80px'
+					PercentGap='-37px'
 					percent='85%'
 					title='ECommerce'
 					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
 				/>
 				<WhatWeDo
-					circle='50px'
+					PercentGap='-10px'
 					percent='99%'
 					title='Support'
 					text='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
--- components/WhatWeDo.js
+++ components/WhatWeDo.js
@@ -1,15 +1,13 @@
-import PercentCircle from './PercentCircle';
+import Circle from './Circle';
 import styles from './WhatWeDo.module.css';
 
 export default function WhatWeDo(props) {
-	const { title, text, percent, circle } = props;
+	const { title, text, percent, PercentGap } = props;
 	return (
 		<div className={styles.WhatWeDo}>
-			<PercentCircle
-				percent={percent}
-				circle={circle}
-				className={styles.PercentCircle}
-			/>
+			<div className={styles.CircleWrap}>
+				<Circle percent={percent} PercentGap={PercentGap} />
+			</div>
 			<div className={styles.TextWrap}>
 				<h3 className={styles.Title}>{title}</h3>
 				<p className={styles.Text}>{text}</p>
