220d82575d6b4c7c0137722abdf76b9aeebf7a0c
Erick Askim <erickaskim@gmail.com>
2021-11-01 11:21:34 -0700

    data showing

--- views/Blog.js
+++ views/Blog.js
@@ -1,139 +1,31 @@
-import Link from 'next/link';
-import {
-	FacebookIcon,
-	FacebookShareButton,
-	TwitterIcon,
-	TwitterShareButton,
-} from 'react-share';
+import Head from 'next/head';
 import Header from '../components/Header';
+import { attributes, react as HomeContent } from '../content/posts.md';
 import styles from './Blog.module.css';
-import ArrowRight from '../public/arrowright.svg';
-import Breadcrumbs from '../components/Breadcrumbs';
 
 export default function blog() {
+	let { title, posts, body, date } = attributes;
 	return (
 		<div className={styles.TopWrapper}>
-			<Header />
+			<Head>
+				<script src='https://identity.netlify.com/v1/netlify-identity-widget.js'></script>
+			</Head>
 			<div className={styles.BottomWrapper}>
-				<div className={styles.items}>
-					<div className={styles.PageTitleWrapper}>
-						<h2 className={styles.PageTitle}>BLOG</h2>
-						<div className={styles.Breadcrumbs}>
-							<Breadcrumbs page='Blog' />
-						</div>
-					</div>
-					<div className={styles.PreviewWrap}>
-						<div className={styles.ImageWrap}>
-							<img src='https://picsum.photos/200' layout='fill' alt='image' />
-						</div>
-						<div className={styles.PostAuthorDataWrap}>
-							<div className={styles.TextWrap}>
-								<p className={styles.Text}>Erick Askim</p>
-								<p className={styles.Text}>Aug 12,2021</p>
-							</div>
-							<FacebookShareButton url='#'>
-								<FacebookIcon size={32} round={true} />
-							</FacebookShareButton>
-							<TwitterShareButton url='#'>
-								<TwitterIcon size={32} round={true} />
-							</TwitterShareButton>
-						</div>
-						<h3 className={styles.Title}>Lorem ipsum</h3>
-						<Link href='/post'>
-							<a aria-label='Next'>
-								<ArrowRight className={styles.ArrowRight} />
-							</a>
-						</Link>
-					</div>
-					<div className={styles.PreviewWrap}>
-						<div className={styles.ImageWrap}>
-							<img src='https://picsum.photos/200' layout='fill' alt='image' />
-						</div>
-						<div className={styles.PostAuthorDataWrap}>
-							<div className={styles.TextWrap}>
-								<p className={styles.Text}>Erick Askim</p>
-								<p className={styles.Text}>Aug 12,2021</p>
-							</div>
-							<FacebookShareButton url='#'>
-								<FacebookIcon size={32} round={true} />
-							</FacebookShareButton>
-							<TwitterShareButton url='#'>
-								<TwitterIcon size={32} round={true} />
-							</TwitterShareButton>
-						</div>
-						<h3 className={styles.Title}>Lorem ipsum</h3>
-						<Link href='/post'>
-							<a aria-label='Next'>
-								<ArrowRight className={styles.ArrowRight} />
-							</a>
-						</Link>
-					</div>
-					<div className={styles.PreviewWrap}>
-						<div className={styles.ImageWrap}>
-							<img src='https://picsum.photos/200' layout='fill' alt='image' />
-						</div>
-						<div className={styles.PostAuthorDataWrap}>
-							<div className={styles.TextWrap}>
-								<p className={styles.Text}>Erick Askim</p>
-								<p className={styles.Text}>Aug 12,2021</p>
-							</div>
-							<FacebookShareButton url='#'>
-								<FacebookIcon size={32} round={true} />
-							</FacebookShareButton>
-							<TwitterShareButton url='#'>
-								<TwitterIcon size={32} round={true} />
-							</TwitterShareButton>
-						</div>
-						<h3 className={styles.Title}>Lorem ipsum</h3>
-						<Link href='/post'>
-							<a aria-label='Next'>
-								<ArrowRight className={styles.ArrowRight} />
-							</a>
-						</Link>
-					</div>
-					<div className={styles.PreviewWrap}>
-						<div className={styles.ImageWrap}>
-							<img src='https://picsum.photos/200' layout='fill' alt='image' />
-						</div>
-						<div className={styles.PostAuthorDataWrap}>
-							<div className={styles.TextWrap}>
-								<p className={styles.Text}>Erick Askim</p>
-								<p className={styles.Text}>Aug 12,2021</p>
-							</div>
-							<FacebookShareButton url='#'>
-								<FacebookIcon size={32} round={true} />
-							</FacebookShareButton>
-							<TwitterShareButton url='#'>
-								<TwitterIcon size={32} round={true} />
-							</TwitterShareButton>
-						</div>
-						<h3 className={styles.Title}>Lorem ipsum</h3>
-						<Link href='/post'>
-							<a aria-label='Next'>
-								<ArrowRight className={styles.ArrowRight} />
-							</a>
-						</Link>
-					</div>
-				</div>
-				<div className={styles.PaginationWrap}>
-					<ul className={styles.PaginationUl}>
-						<li className={styles.PaginationLi}>
-							<Link href='/post'>
-								<a className={styles.Link}>1</a>
-							</Link>
-						</li>
-						<li className={styles.PaginationLi}>
-							<Link href='/post'>
-								<a className={styles.Link}>2</a>
-							</Link>
-						</li>
-						<li className={styles.PaginationLi}>
-							<Link href='/post'>
-								<a className={styles.Link}>3</a>
-							</Link>
-						</li>
+				<Header />
+				<article>
+					<h1>{title}</h1>
+					<HomeContent />
+					<p>{body}</p>
+					<p>{date}</p>
+					<ul>
+						{posts.map((post, k) => (
+							<li key={k}>
+								<h2>{post.name}</h2>
+								<p>{post.description}</p>
+							</li>
+						))}
 					</ul>
-				</div>
+				</article>
 			</div>
 		</div>
 	);
