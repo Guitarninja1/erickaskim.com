4c29c0c14bfc38c48825cc3ef84c4fccffa41af0
Erick Askim <erickaskim@gmail.com>
2021-11-01 11:31:53 -0700

    data loading

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@ module.exports = {
 	},
 	webpack(config) {
 		config.module.rules.push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack'],
+			test: /\.md$/,
+			loader: 'frontmatter-markdown-loader',
+			options: { mode: ['react-component'] },
 		});
 
 		return config;
